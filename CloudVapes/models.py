656875c9a0747ff767c2760d21e20500a52fc637
Marko Srbakov <85948841+markosrbakov@users.noreply.github.com>
2025-05-09 19:08:30 +0200

    Update models.py

--- CloudVapes/models.py
+++ CloudVapes/models.py
@@ -4,8 +4,23 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
+    FLAVOUR_CHOICES = [
+        ('DRAGON_FRUIT_ICE', 'Dragon Fruit Ice'),
+        ('BLUESOUR_RASPBERRY', 'Blue Sour Raspberry'),
+        ('STRAWBERRY_KIWI', 'Strawberry Kiwi'),
+        ('BLUEBERRY_ON_ICE', 'Blueberry on Ice'),
+        ('MIXED_BERRIES', 'Mixed Berries'),
+        ('DR_BLUE', 'Dr Blue'),
+        ('LOVE_66', 'Love 66'),
+        ('PINK_LEMONADE', 'Pink Lemonade'),
+        ('BERRY_LEMONADE', 'Berry Lemonade'),
+    ]
     price = models.DecimalField(max_digits=6, decimal_places=2)
     quantity = models.PositiveIntegerField()
+    # flavour = models.CharField(max_length=50, choices=FLAVOUR_CHOICES)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
         full_name = models.CharField(max_length=100)
