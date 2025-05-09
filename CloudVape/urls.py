4863c078b1fdd52681675e413b8df0009f4d4664
Marko Srbakov <85948841+markosrbakov@users.noreply.github.com>
2025-05-09 16:52:13 +0200

    Update urls.py

--- CloudVape/urls.py
+++ CloudVape/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from CloudVapes import views
+from CloudVapes.views import  contact_view
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('submit-order/', views.submit_order, name='submit_order'),
+    path('contact/', contact_view, name='contact'),
 ]
 
 if settings.DEBUG:
